19d8a40e1545bcbaa59e3cababb9ca34bbc0f8e0
Champoolwoo <golf_zet@hotmail.com>
2017-05-04 12:30:42 +0700

    date

--- public/app.js
+++ public/app.js
@@ -7,93 +7,13 @@ angular.module('app', [])
 		$scope.sheets = []
 		$scope.status = 44
 
-		$scope.box1 = ''
-		$scope.box2 = ''
-		$scope.box3 = ''
-		$scope.box4 = ''
-		$scope.box5 = ''
-		$scope.box6 = ''
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////control
 
-		setInterval(function(){
-
-			var start  = new Date()
-			var end1  = new Date($scope.box1)
-			var end2  = new Date($scope.box2)
-			var end3  = new Date($scope.box3)
-			var end4  = new Date($scope.box4)
-			var end5  = new Date($scope.box5)
-			var end6  = new Date($scope.box6)
-
-			var diff1 = end1- start
-			var diff2 = end2- start
-			var diff3 = end3- start
-			var diff4 = end4- start
-			var diff5 = end5- start
-			var diff6 = end6- start
-
-			var dd1  = Math.floor( diff1 / (1000*60*60*24) )
-			var hh1 = Math.floor( diff1 / (1000*60*60) )
-			var mm1 = Math.floor( diff1 / (1000*60) )
-			var ss1 = Math.floor( diff1 / 1000 )
-			$scope.day1 = dd1
-			$scope.hour1 = hh1 - dd1 * 24
-			$scope.min1 = mm1 - hh1 * 60
-			$scope.sec1 = ss1 - mm1 * 60
-
-			var dd2  = Math.floor( diff2 / (1000*60*60*24) )
-			var hh2 = Math.floor( diff2 / (1000*60*60) )
-			var mm2 = Math.floor( diff2 / (1000*60) )
-			var ss2 = Math.floor( diff2 / 1000 )
-			$scope.day2 = dd2
-			$scope.hour2 = hh2 - dd2  * 24
-			$scope.min2 = mm2 - hh2 * 60
-			$scope.sec2 = ss2 - mm2  * 60
-
-			var dd3  = Math.floor( diff3 / (1000*60*60*24) )
-			var hh3 = Math.floor( diff3 / (1000*60*60) )
-			var mm3 = Math.floor( diff3 / (1000*60) )
-			var ss3 = Math.floor( diff3 / 1000 )
-			$scope.day3 = dd3
-			$scope.hour3 = hh3 - dd3  * 24
-			$scope.min3 = mm3 - hh3 * 60
-			$scope.sec3 = ss3 - mm3  * 60
-
-			var dd4  = Math.floor( diff4 / (1000*60*60*24) )
-			var hh4 = Math.floor( diff4 / (1000*60*60) )
-			var mm4 = Math.floor( diff4 / (1000*60) )
-			var ss4 = Math.floor( diff4 / 1000 )
-			$scope.day4 = dd4
-			$scope.hour4 = hh4 - dd4  * 24
-			$scope.min4 = mm4 - hh4 * 60
-			$scope.sec4 = ss4 - mm4  * 60
-
-			var dd5  = Math.floor( diff5 / (1000*60*60*24) )
-			var hh5 = Math.floor( diff5 / (1000*60*60) )
-			var mm5 = Math.floor( diff5 / (1000*60) )
-			var ss5 = Math.floor( diff5 / 1000 )
-			$scope.day5 = dd5
-			$scope.hour5 = hh5 - dd5  * 24
-			$scope.min5 = mm5 - hh5 * 60
-			$scope.sec5 = ss5 - mm5  * 60
-
-			var dd6  = Math.floor( diff6 / (1000*60*60*24) )
-			var hh6 = Math.floor( diff6 / (1000*60*60) )
-			var mm6 = Math.floor( diff6 / (1000*60) )
-			var ss6 = Math.floor( diff6 / 1000 )
-			$scope.day6 = dd6
-			$scope.hour6 = hh6 - dd6  * 24
-			$scope.min6 = mm6 - hh6 * 60
-			$scope.sec6 = ss6 - mm6  * 60
-
-			$scope.$apply()
-			// console.log($scope.day1+" Day "+$scope.hour1+" Hours")
-			// console.log($scope.day2+" Day "+$scope.hour2+" Hours")
-			// console.log($scope.day3+" Day "+$scope.hour3+" Hours")
-			// console.log($scope.day4+" Day "+$scope.hour4+" Hours")
-			// console.log($scope.day5+" Day "+$scope.hour5+" Hours")
-			// console.log($scope.day6+" Day "+$scope.hour6+" Hours")
-
-		 }, 1000)//setInterval
+		$http.get('https://sheetsu.com/apis/v1.0/5eac309ffcdc').then(function(res){
+			console.log()
+		      $scope.items = res.data[res.data.length - 1]
+		      $scope.sheets = res.data
+    		})
 
 		$scope.ledOn = function(){
 			$http.get('/ledOn').then(function (response){
@@ -127,92 +47,184 @@ angular.module('app', [])
 			console.log("Take Photo")
 		}
 
-  // 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  /*setInterval(function(){
-  	// var date.timezone = Asia/Bangkok
-  	var a = new Date($scope.box1)
-  	var x = new Date()
-  	var z = x.getTime()
-  	var b = a.getTime()
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////control
 
-    var start = moment(Date.now())
-    var end1 = moment($scope.box1)
-    var end2 = moment($scope.box2)
-    var end3 = moment($scope.box3)
-    var end4 = moment($scope.box4)
-    var end5 = moment($scope.box5)
-    var end6 = moment($scope.box6)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////date2
 
-    var milisec1 = end1.diff(start)
-    var milisec2 = end2.diff(start)
-    var milisec3 = end3.diff(start)
-    var milisec4 = end4.diff(start)
-    var milisec5 = end5.diff(start)
-    var milisec6 = end6.diff(start)
+		$scope.dateEndDate = []
 
-    $scope.day1 = moment.duration(milisec1).days()
-    $scope.hour1 = moment.duration(milisec1).hours()
-    $scope.min1 = moment.duration(milisec1).minutes()
-    $scope.sec1 = moment.duration(milisec1).seconds()
+		$scope.eat = function(){
+			var dateforpush = {}
+			var end = moment().add(35, 'days').calendar()
 
-    $scope.day2 = moment.duration(milisec2).days()
-    $scope.hour2 = moment.duration(milisec2).hours()
-    $scope.min2 = moment.duration(milisec2).minutes()
-    $scope.sec2 = moment.duration(milisec2).seconds()
-
-    $scope.day3 = moment.duration(milisec3).days()
-    $scope.hour3 = moment.duration(milisec3).hours()
-    $scope.min3 = moment.duration(milisec3).minutes()
-    $scope.sec3 = moment.duration(milisec3).seconds()
-
-    $scope.day4 = moment.duration(milisec4).days()
-    $scope.hour4 = moment.duration(milisec4).hours()
-    $scope.min4 = moment.duration(milisec4).minutes()
-    $scope.sec4 = moment.duration(milisec4).seconds()
-
-    $scope.day5 = moment.duration(milisec5).days()
-    $scope.hour5 = moment.duration(milisec5).hours()
-    $scope.min5 = moment.duration(milisec5).minutes()
-    $scope.sec5 = moment.duration(milisec5).seconds()
-
-    $scope.day6 = moment.duration(milisec6).days()
-    $scope.hour6 = moment.duration(milisec6).hours()
-    $scope.min6 = moment.duration(milisec6).minutes()
-    $scope.sec6 = moment.duration(milisec6).seconds()
-    // console.log($scope.box1)
-    // console.log($scope.day1 +" "+ $scope.hour1 +" "+  $scope.min1 +" "+ $scope.sec1)
-     $scope.$apply()
-
-    // $scope.hour = moment.duration(start).hours()
-    // $scope.min = moment.duration(start).minutes()
-    // $scope.sec = moment.duration(start).seconds()
-     // console.log($scope.box1)
-    // console.log(start)
-    // console.log($scope.day, $scope.hour, $scope.min, $scope.sec)
-  }, 1000)//setInterval*/
-  // 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		$scope.controlSwitch = function(){
-			if($scope.conLED == true){
-				console.log("LED On")
-				$scope.LED = true
-			}else{
-				console.log("LED Off")
-				$scope.LED = false
+			var dateforpush = {
+				end: end
 			}
 
-			if($scope.conPump == true){
-				console.log("Pump On")
-			}else{
-				console.log("Pump Off")
-			}
+			$http.post('/dateend', dateforpush).then(function (response){
+				// $scope.dateEndDate.push(response)
+			})
 		}
 
-		$http.get('https://sheetsu.com/apis/v1.0/5eac309ffcdc').then(function(res){
-			console.log()
-		      $scope.items = res.data[res.data.length - 1]
-		      $scope.sheets = res.data
-    		})
+		$scope.getdataeat = function(){
+			$http.get('/dateend').then(function (response){
+				$scope.dateEndDate = response.data
+			})
+		}
 
+		 // test tak
+		$scope.hole = [
+			{
+				hole: 1,
+				status: false
+			},
+			{
+				hole: 2,
+				status: false
+			},
+			{
+				hole: 3,
+				status: false
+			}
+		]
+
+		$scope.totalHole = []
+		getHole()
+		function getHole () {
+			$http.get('/dateend').then(res => {
+				$scope.totalHole = res.data
+				// console.log($scope.totalHole)
+			})
+		}
+
+		 $scope.tree = function (hole) {
+		 	var index =  $scope.hole.findIndex(i => i.hole === hole)
+		 	if ($scope.hole[index].status === false) {
+		 		console.log('first')
+		 		$scope.hole[index].status = true
+		 		$scope.hole[index].end =  moment().add(35, 'days').calendar()
+		 		var cur  = $scope.totalHole.findIndex(i => i.hole === hole)
+
+		 		console.log(cur)
+		 		if (cur >= 0) {
+		 			console.log('th')
+		 			var cur  = $scope.totalHole.findIndex(i => i.hole === hole)
+		 			$scope.totalHole[cur].status = true
+		 			// console.log('xxxx ', $scope.totalHole[cur])
+		 			$http.put('/dateend/' +  $scope.totalHole[cur]._id, $scope.totalHole[cur]).then(res => {})
+		 		} else if (cur === -1) {
+		 			console.log('sec')
+		 			// $scope.hole[index].status = false
+		 			// console.log('yyy ', $scope.hole[index])
+		 			$http.post('/dateend', $scope.hole[index]).then(function (response){})
+		 		}else {
+		 			$http.post('/dateend', $scope.hole[index]).then(function (response){})
+		 		}
+		 		getHole()
+		 	}else {
+		 		console.log('four')
+		 		var _id = ''
+		 		$scope.hole[index].status = false
+		 		$http.get('/dateend').then(res => {
+		 			$scope.dateEndDate = res.data
+		 			res.data.findIndex(i => {
+		 				if (i.hole === hole)  {
+		 					_id =  i._id
+		 					// console.log('id ', i._id)
+		 				}
+		 			})
+		 		}).then(() => {
+		 			// console.log($scope.hole[index])
+		 			$scope.hole[index].status = false
+		 			$http.put('/dateend/' +  _id, $scope.hole[index]).then(res => {
+		 			})
+		 		})
+		 	}//else
+		 }//function
+
+		$scope.getdataeat = function(){
+			$http.get('/dateend').then(function (response){
+				$scope.dateEndDate1 = response.data[0].end
+				$scope.dateEndDate2 = response.data[1].end
+				$scope.dateEndDate3 = response.data[2].end
+
+				var start = moment()
+				var dayend1 = moment($scope.dateEndDate1)
+				var dayend2 = moment($scope.dateEndDate2)
+				var dayend3 = moment($scope.dateEndDate3)
+
+				$scope.calday1 = dayend1.diff(start, 'days')
+				$scope.calday2 = dayend2.diff(start, 'days')
+				$scope.calday3 = dayend3.diff(start, 'days')
+
+				// $scope.calday = $scope.dateEndDate.find().end
+				console.log($scope.dateEndDate1)
+				console.log($scope.calday1)
+
+			})
+		}
+
+		$scope.getdataeat()
+		 // // test tak
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////date2
 })
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////ton
+///
+	/*$scope.eat = function(){
+			var dateforpush = {}
+
+			var end = moment().add(35, 'days').calendar()
+
+			var dateforpush = {
+				end: end
+			}
+			$http.post('/dateend', dateforpush).then(function (response){
+				// $scope.dateEndDate.push(response)
+			})
+		}
+
+		$scope.getdataeat = function(){
+			$http.get('/dateend').then(function (response){
+
+				$scope.dateEndDate = response.data
+								// console.log($scope.dateEndDate)
+			})
+		}*/
+
+// $scope.getdataeat()
+
+		/*$scope.countDate = function (endDate){
+			// $scope.calbox = '0'
+				var start = moment()
+				var enddaybox =  endDate
+				// console.log(enddaybox)
+				var endbox = moment(enddaybox)
+
+				// console.log(endbox)
+
+				// $scope.calbox = endbox.diff(start, 'days')
+				// console.log($scope.calbox)
+				return endbox.diff(start, 'days')
+		}*/
+
+		/*$scope.reset = function (index) {
+
+			var dateforpush = {}
+			var end = moment().add(35, 'days').calendar()
+
+			var dateforpush = {
+				end: end
+			}
+
+			$http.put('/dateend/' + index, dateforpush).then(function (res) {
+      			console.log(res.data)
+      			$scope.dateEndDate.end = res.data.end
+    		})
+		}
+
+		$scope.countDate()*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////ton
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var PyhonShell = require('python-shell')
-var  mongoose = require('mongoose')
+var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var Schema = mongoose.Schema
 var thingSchema = new Schema({}, {strict: false})
 var DateEat = mongoose.model('SmartFarm', thingSchema)
 var app = express()
 
-mongoose.connect('mongodb://farm:26032538@ds127531.mlab.com:27531/farm')
+mongoose.connect('mongodb://localhost:27017/farm')
+
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
@@ -46,13 +47,39 @@ app.get('/takeCam', function(req, res, next){
 	})
 })
 
-app.get('/api', function(res, req){
-	DateEat.find({})
-	.exec(function (err,done){
-			if(err) console.log(err)
-			res.send(done)
+//////////////////////////////////////////////////////////////////////////////////////
+app.post('/dateend', function(req, res){
+	let obj = new DateEat(req.body)
+	obj.save(function(err, obj){
+		if(err){
+			return(res.send(err))
+		}else{
+			return(res.send('done'))
+		}
 	})
 })
+
+app.get('/dateend', function (req, res) {
+    DateEat.find({})
+    .exec(function (err, done) {
+      if (err) console.log(err)
+      res.send(done)
+    })
+  })
+
+app.put('/dateend/:id', function (req, res) {
+    DateEat.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: {end: req.body.end, hole: req.body.hole, status: req.body.status} },
+      { new: true })
+      .exec(function (err, done) {
+        if (err) console.log(err)
+        res.send(done)
+      })
+  })
+
+
+
 
 app.listen(3000)
 console.log('running on port 3000.')
