8518e0e2b8d8f8f9757a5310e14cbf8ac888b4e8
Champoolwoo <golf_zet@hotmail.com>
2017-05-06 05:29:21 +0700

    trak And tack cam

--- takeCam.py
+++ takeCam.py
@@ -6,4 +6,4 @@ GPIO.setmode(GPIO.BCM)
 
 #-------------------
 
-os.system("fswebcam -d /dev/video0 -r 1280x780 --no-banner Front1.jpg")
+os.system("fswebcam -d /dev/video0 -r 1280x780 --no-banner /picture/cam1/Floor1.jpg")
