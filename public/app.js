c6d477d4707490b8f4f73fc347ada2eca339a4db
Champoolwoo <golf_zet@hotmail.com>
2017-05-08 02:59:32 +0700

    btn

--- public/app.js
+++ public/app.js
@@ -220,7 +220,6 @@ angular.module('app', [])
 		$scope.tree = function (hole) {
 
 		 	var index =  $scope.hole.findIndex(i => i.hole === hole)
-		 	$http.post('/dateend', $scope.hole[index]).then(function (response){})
 
 		 	if ($scope.totalHole[index].status === false) {
 		 		console.log('first')
