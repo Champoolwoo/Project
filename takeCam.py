2c7a0ba455789f8bc34ca4ae3a17e81b0105e3e8
Champoolwoo <golf_zet@hotmail.com>
2017-05-08 15:44:35 +0700

    cam

--- takeCam.py
+++ takeCam.py
@@ -7,6 +7,6 @@ GPIO.setmode(GPIO.BCM)
 #-------------------
 
 os.system("fswebcam -s 15  -p YUYV -d /dev/video1 -r 1280x780 --no-banner public/picture/cam1/Floor1.jpg")
-os.system("fswebcam -s 15  -p YUYV -d /dev/video007 -r 1280x780 --no-banner public/picture/cam2/Floor2.jpg")
+os.system("fswebcam -s 15  -p YUYV -d /dev/video2 -r 1280x780 --no-banner public/picture/cam2/Floor2.jpg")
 os.system("fswebcam -s 15  -p YUYV -d /dev/video009 -r 1280x780 --no-banner public/picture/cam3/Floor3.jpg")
 
