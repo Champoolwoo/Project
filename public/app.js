de1189885d5a1289bcb3616362daf989cc248a03
champoolwoo <golf_zet@hotmail.com>
2017-08-09 14:55:13 +0700

    man auto

--- public/app.js
+++ public/app.js
@@ -10,7 +10,8 @@ angular.module('app', [])
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Data Googlesheets
 //https://sheetsu.com/apis/v1.0/5eac309ffcdc
 //https://sheetsu.com/apis/v1.0/b7e3ea00dedb
-		$http.get('https://sheetsu.com/apis/v1.0/b7e3ea00dedb').then(function(res){
+//https://sheetsu.com/apis/v1.0/e36e82f2bcd1
+		$http.get('https://sheetsu.com/apis/v1.0/e36e82f2bcd1').then(function(res){
 			console.log()
 		      $scope.items = res.data[res.data.length - 1]
 		      $scope.sheets = res.data
